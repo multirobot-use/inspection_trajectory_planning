121cafac3e641b93f7ce1f904e1648965532072d
damianpm98 <damianperez8@gmail.com>
2021-03-09 13:01:46 +0100

    Basic hpp and cpp for mission planner

--- packages/mission_planner/include/mission_planner_ros.hpp
+++ packages/mission_planner/include/mission_planner_ros.hpp
@@ -1,12 +1,28 @@
 #pragma once
 #include "mission_planner.hpp"
 #include "ros/ros.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 
 class MissionPlannerRos {
- private:
-  ros::NodeHandle nh_;
+  private:
 
- public:
-  MissionPlannerRos(ros::NodeHandle _nh);
-  ~MissionPlannerRos();
+    // Definitions
+    ros::NodeHandle nh_;
+
+    int                           drone_id_;  // From launch
+    geometry_msgs::PoseStamped     cur_pose;
+    geometry_msgs::TwistStamped    cur_vel;
+
+    // Subscriptions
+    ros::Subscriber               cur_pose_sub;
+    ros::Subscriber               cur_vel_sub;
+
+    // Callbacks prototypes
+    void uavPoseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg);
+    void uavVelocityCallback(const geometry_msgs::TwistStamped::ConstPtr &msg);
+
+  public:
+    MissionPlannerRos(ros::NodeHandle _nh);
+    ~MissionPlannerRos();
 };
--- packages/mission_planner/src/mission_planner_ros.cpp
+++ packages/mission_planner/src/mission_planner_ros.cpp
@@ -1,5 +1,35 @@
 #include "mission_planner_ros.hpp"
 
-MissionPlannerRos::MissionPlannerRos(ros::NodeHandle _nh) : nh_(_nh) {}
+MissionPlannerRos::MissionPlannerRos(ros::NodeHandle _nh) : nh_(_nh) {
+  // Subscribers
+  cur_pose_sub      = nh_.subscribe<geometry_msgs::PoseStamped>("/drone_1/ual/pose", 1, &MissionPlannerRos::uavPoseCallback, this); 
+  cur_vel_sub       = nh_.subscribe<geometry_msgs::TwistStamped>("/drone_1/ual/velocity", 1, &MissionPlannerRos::uavVelocityCallback, this); 
+
+}
 
 MissionPlannerRos::~MissionPlannerRos() {}
+
+
+// Callbacks
+void MissionPlannerRos::uavPoseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg){
+    cur_pose.pose.position.x = msg->pose.position.x;
+    cur_pose.pose.position.y = msg->pose.position.y;
+    cur_pose.pose.position.z = msg->pose.position.z; 
+
+    cur_pose.pose.orientation.x = msg->pose.orientation.x;
+    cur_pose.pose.orientation.y = msg->pose.orientation.y;
+    cur_pose.pose.orientation.z = msg->pose.orientation.z;
+    cur_pose.pose.orientation.w = msg->pose.orientation.w;
+
+}
+
+void MissionPlannerRos::uavVelocityCallback(const geometry_msgs::TwistStamped::ConstPtr &msg){
+    cur_vel.twist.linear.x  = msg->twist.linear.x;
+    cur_vel.twist.linear.y  = msg->twist.linear.y;
+    cur_vel.twist.linear.z  = msg->twist.linear.z;
+
+    cur_vel.twist.angular.x     = msg->twist.angular.x;
+    cur_vel.twist.angular.y     = msg->twist.angular.y;
+    cur_vel.twist.angular.z     = msg->twist.angular.z;
+
+}
