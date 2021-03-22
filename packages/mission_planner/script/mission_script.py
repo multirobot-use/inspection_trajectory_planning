9eb94ae46bb9a57285f810546c5e2c5f448c95ad
damianpm98 <damianperez8@gmail.com>
2021-03-22 12:49:49 +0100

    Added menu and functions to the interface script

--- packages/mission_planner/script/mission_script.py
+++ packages/mission_planner/script/mission_script.py
@@ -11,28 +11,192 @@ from std_srvs.srv import Empty
 from uav_abstraction_layer.srv import TakeOff
 from uav_abstraction_layer.srv import TakeOffRequest
 from uav_abstraction_layer.srv import GoToWaypoint
+from uav_abstraction_layer.srv import GoToWaypointRequest
 from mission_planner.srv import WaypointSrvRequest
 from mission_planner.srv import WaypointSrv
 
 
-# Main function
-if __name__ == "__main__":
+# Menu function
+def show_menu():
+    print "\n\nWelcome to the main menu. Put the number of the desired option:\n"
+    print "\t1. Take off and send the drones to their initial points (WORKING)"
+    print "\t2. Start the mission"
+    print "\t3. Stop the mission"
+    print "\t4. Add waypoint (WORKING)"
+    print "\t5. Clear all the waypoints (WORKING)"
+    print "\t6. Change relative angles between followers and leader"
+    print "\t7. Change distance to inspection point"
+    print "\t8. Change height to inspection point\n"
     
-    # Create the node
-    rospy.init_node("operator", anonymous=True)
+    option = int(raw_input (">> "))
+    while (option < 1 or option > 8):
+        option = raw_input("Please, choose a valid option: ")
+
+    if option == 1:
+        preparing_drones()
+    elif option == 2:
+        start_mission()
+    elif option == 3:
+        stop_mission()
+    elif option == 4:
+        add_one_waypoint()
+    elif option == 5:
+        clear_all_waypoints()
+    elif option == 6:
+        relative_angle()
+    elif option == 7:
+        distance_inspection()
+    elif option == 8:
+        height_inspection()
+    else:
+        print ("Option n" + str(option) + "does not exist!")
     
+
+# 1.        preparing_drones function
+# Brief:    Takes off all the drones and send them to the initial point
+# NOTE: Not finished
+def preparing_drones():
+    print "Preparing drones called"
+    
+    # DRONE 1 - LEADER
     # Only the leader drone guidable
     uav_id = "drone_1"
     ns     = "/drone_1"
     
     # TakeOff service
     take_off_service = rospy.ServiceProxy(ns+"/ual/take_off", TakeOff)
-    #
     
     # GoToWaypoint service
-    go_to_waypoint_url = "/drone_1/ual/go_to_waypoint"
+    go_to_waypoint_url      = "/drone_1/ual/go_to_waypoint"
     rospy.wait_for_service(go_to_waypoint_url)
-    go_to_waypoint = rospy.ServiceProxy(go_to_waypoint_url, GoToWaypoint)
+    go_to_waypoint_service  = rospy.ServiceProxy(go_to_waypoint_url, GoToWaypoint)
+    
+    # DRONE n - FOLLOWERS
+    # TODO
+    
+    
+    # Taking off
+    try:
+        take_off            = TakeOffRequest()
+        take_off.height     = 1
+        take_off.blocking   = False
+        
+        # Leader drone service
+        take_off_service(take_off)
+        
+        # Followers service
+        # TODO
+        
+        print "LEADER: Taking off the drone"
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" %e
+    
+    # Send to the initial point
+    # TODO: NEED TO GET THE INITIAL POINT FROM SOMEWHERE
+    initial_point = [3, 4, 5]
+    
+    # Temporary fix: need to know if the drone has already reached the desired height of the take off
+    time.sleep(7)
+    
+    # Going to initial point
+    try:
+        waypoint              = GoToWaypointRequest()
+        waypoint.blocking     = False
+        waypoint.waypoint.pose.position.x     = initial_point[0]
+        waypoint.waypoint.pose.position.y     = initial_point[1]
+        waypoint.waypoint.pose.position.z     = initial_point[2]
+        
+        go_to_waypoint_service(waypoint)
+        print "LEADER: Going to initial waypoint"
+    
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" %e
+
+
+# 2.        start_mission function
+# Brief:    This function allows the drones to start the mission
+# TODO
+def start_mission():
+    pass
+
+
+# 3.        stop_mission function
+# Brief:    This function stops mission
+# TODO
+def stop_mission():
+    pass
+
+
+# 4.        add_one_waypoint function
+# Brief:    This function adds one waypoint to the queue of waypoints that the drone has to reach
+def add_one_waypoint():
+    px = float(raw_input("X pose: "))
+    py = float(raw_input("Y pose: "))
+    pz = float(raw_input("Z pose: "))
+    
+    tx = float(raw_input("X linear velocity: "))
+    ty = float(raw_input("Y linear velocity: "))
+    tz = float(raw_input("Z linear velocity: "))
+    
+    add_waypoint_req = WaypointSrvRequest()
+    
+    add_waypoint_req.waypoint.pose.pose.position.x      = px
+    add_waypoint_req.waypoint.pose.pose.position.y      = py
+    add_waypoint_req.waypoint.pose.pose.position.z      = pz
+    
+    add_waypoint_req.waypoint.twist.twist.linear.x      = tx
+    add_waypoint_req.waypoint.twist.twist.linear.y      = ty
+    add_waypoint_req.waypoint.twist.twist.linear.z      = tz
+        
+    try:
+        add_waypoint_service(add_waypoint_req)
+        print "Waypoint added"
+    except:
+        print("Failed calling add_waypoint service")
+
+
+# 5.        clear_all_waypoints function
+# Brief:    This function clears the queue of desired waypoints
+def clear_all_waypoints():
+    try:
+        clear_waypoints_service()
+    except:
+        print("Failed calling clear_waypoints service")
+
+
+# 6.        relative_angle function
+# Brief:    This function allows to change the relative angle between the leader and the followers
+# TODO
+def relative_angle():
+    pass
+
+
+# 7.        relative_angle function
+# Brief:    This function changes the distance between the inspection point and the drones
+# TODO
+def distance_inspection():
+    pass
+
+
+# 8.        height_inspection function
+# Brief:    This function changes the desired height to inspect
+# TODO
+def height_inspection():
+    pass
+
+
+
+
+
+
+
+
+    
+# Main function
+if __name__ == "__main__":
+    
+    # Create the node
+    rospy.init_node("operator", anonymous=True)
     
     # Mission planner services
     activate_planner_url = "/drone_1/mission_planner_ros/activate_planner" # Missing its functionality atm
@@ -41,67 +205,8 @@ if __name__ == "__main__":
     
     activate_planner_service = rospy.ServiceProxy(activate_planner_url, SetBool)
     add_waypoint_service     = rospy.ServiceProxy(add_waypoint_url, WaypointSrv)
-    clear_waypoints_service  = rospy.ServiceProxy(clear_waypoints_url, Empty)    
+    clear_waypoints_service  = rospy.ServiceProxy(clear_waypoints_url, Empty)
     
-    # Taking off the drone
-    try:
-        take_off            = TakeOffRequest()
-        take_off.height     = 1
-        take_off.blocking   = False
-        take_off_service(take_off)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s" %e
-    
-    raw_input("Put ENTER to order the first command: add one waypoint")
-    add_waypoint_req = WaypointSrvRequest()
-    
-    add_waypoint_req.waypoint.pose.pose.position.x   = 15
-    add_waypoint_req.waypoint.pose.pose.position.y   = 35
-    add_waypoint_req.waypoint.pose.pose.position.z   = 10
-    
-    add_waypoint_req.waypoint.twist.twist.linear.x  = 1.5
-    add_waypoint_req.waypoint.twist.twist.linear.y  = 2.5
-    add_waypoint_req.waypoint.twist.twist.linear.z  = 0.5
-        
-    try:
-        add_waypoint_service(add_waypoint_req)
-    except:
-        print("Failed calling add_waypoint service")
-    
-    raw_input("Put ENTER to order the second command: add two more waypoints")
-    
-    # First waypoint
-    add_waypoint_req.waypoint.pose.pose.position.x   = 3
-    add_waypoint_req.waypoint.pose.pose.position.y   = 42
-    add_waypoint_req.waypoint.pose.pose.position.z   = 6
-    
-    add_waypoint_req.waypoint.twist.twist.linear.x  = 1.0
-    add_waypoint_req.waypoint.twist.twist.linear.y  = 3.5
-    add_waypoint_req.waypoint.twist.twist.linear.z  = 0.2
-        
-    try:
-        add_waypoint_service(add_waypoint_req)
-    except:
-        print("Failed calling add_waypoint service")
-    
-    time.sleep(1)
-    # Second waypoint
-    add_waypoint_req.waypoint.pose.pose.position.x   = 7
-    add_waypoint_req.waypoint.pose.pose.position.y   = 12
-    add_waypoint_req.waypoint.pose.pose.position.z   = 14
-    
-    add_waypoint_req.waypoint.twist.twist.linear.x  = 0.2
-    add_waypoint_req.waypoint.twist.twist.linear.y  = -2.5
-    add_waypoint_req.waypoint.twist.twist.linear.z  = 0.2
-        
-    try:
-        add_waypoint_service(add_waypoint_req)
-    except:
-        print("Failed calling add_waypoint service")
-    
-    raw_input("Put ENTER to order the third command: clear all the waypoints called")
-    
-    try:
-        clear_waypoints_service()
-    except:
-        print("Failed calling clear_waypoints service")
+    while (not rospy.is_shutdown()):
+        show_menu()
+        time.sleep(1)
