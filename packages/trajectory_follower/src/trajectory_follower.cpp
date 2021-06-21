76d659af8dfc1f9e42b808e56d7245f6a88ac740
alfalcmar <a.alcantara.marin@gmail.com>
2021-06-21 17:00:59 +0200

    avoided some ros msg

--- packages/trajectory_follower/src/trajectory_follower.cpp
+++ packages/trajectory_follower/src/trajectory_follower.cpp
@@ -5,22 +5,20 @@
 #include <handy_tools/pid_controller.h>
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
-#include <std_srvs/SetBool.h>
-#include <tf2/utils.h>  // to convert quaternion to roll-pitch-yaw
+#include <tf2/utils.h>  
 #include <trajectory_msgs/JointTrajectory.h>
-#include <uav_abstraction_layer/GoToWaypoint.h>
-#include <uav_abstraction_layer/TakeOff.h>
 #include <Eigen/Eigen>
-#include <fstream>
-#include <iostream>
+#include <thread>        
+#include <chrono>         
 
 const float YAW_PID_P = 0.4;
 const float YAW_PID_I = 0.02;
 const float YAW_PI_D = 0.0;
 
 struct Trajectory {
-  std::vector<geometry_msgs::PoseStamped> positions;  // trajectory to follow
-  std::vector<geometry_msgs::Twist> velocities;       // trajectory to follow
+  std::vector<Eigen::Vector3f> positions;  // trajectory to follow
+  std::vector<Eigen::Vector3f> velocities;       // trajectory to follow
+  std::vector<Eigen::Quaternionf> orientations;
 };
 struct State {
   Eigen::Vector3f current_pose;
@@ -31,84 +29,19 @@ struct State {
 struct Follower {
   const double look_ahead = 1.0;
   int pose_on_path = 0;
-  const float rate = 0.01;  // seg
+  const float rate = 0.01;  // hz
   const float velocity_error = 0.1;
 
-  float calculateYawDiff(float _desired_yaw, float _current_yaw);
-  int cal_pose_on_path(const std::vector<geometry_msgs::PoseStamped> &positions,
+  float calculateYawDiff(const float _desired_yaw, const float _current_yaw);
+  int cal_pose_on_path(const std::vector<Eigen::Vector3f> &positions,
                        const Eigen::Vector3f &current_pose);
   int cal_pose_look_ahead(
-      const std::vector<geometry_msgs::PoseStamped> &positions);
-  Eigen::Vector3f calculate_vel(const Eigen::Vector3f pose,
-                                const Eigen::Vector3f vel,
+      const std::vector<Eigen::Vector3f> &positions);
+  Eigen::Vector3f calculate_vel(const Eigen::Vector3f &pose,
+                                const Eigen::Vector3f &vel,
                                 const Eigen::Vector3f &current_pose);
 };
 
-
-float Follower::calculateYawDiff(float _desired_yaw, float _current_yaw) {
-  float yaw_diff = _desired_yaw - _current_yaw;
-  while (yaw_diff < -M_PI) yaw_diff += 2 * M_PI;
-  while (yaw_diff > M_PI) yaw_diff -= 2 * M_PI;
-  return yaw_diff;
-}
-
-/** \brief Utility function to calculate the nearest pose on the path
- *  \param positions a path to follow
- *  \return index of the nearest pose on the path
- */
-int Follower::cal_pose_on_path(const std::vector<geometry_msgs::PoseStamped> &positions,
-                     const Eigen::Vector3f &current_pose) {
-  double min_distance = INFINITY;
-  int pose_on_path_id = 0;
-  for (int i = pose_on_path; i < positions.size(); i++) {
-    Eigen::Vector3f pose_on_path = Eigen::Vector3f(
-        positions[i].pose.position.x, positions[i].pose.position.y,
-        positions[i].pose.position.z);
-    if ((current_pose - pose_on_path).norm() < min_distance) {
-      min_distance = (current_pose - pose_on_path).norm();
-      pose_on_path_id = i;
-    }
-  }
-  return pose_on_path_id;
-}
-
-/** \brief Utility function to calculate the look ahead position
- *  \param positions a path to follow
- *  \param look_ahead
- *  \pose_on_path pose from which we apply look ahead
- *  \return look ahead position index
- */
-
-int Follower::cal_pose_look_ahead(
-    const std::vector<geometry_msgs::PoseStamped> &positions) {
-  for (int i = pose_on_path; i < positions.size(); i++) {
-    Eigen::Vector3f aux = Eigen::Vector3f(
-        positions[i].pose.position.x - positions[pose_on_path].pose.position.x,
-        positions[i].pose.position.y - positions[pose_on_path].pose.position.y,
-        positions[i].pose.position.z - positions[pose_on_path].pose.position.z);
-    double distance = aux.norm();
-    if (distance > look_ahead) return i;
-  }
-  return positions.size();
-}
-
-/** \brief utility function to calculate velocity commands. This function apply
- * the direction to the next point of the trajectory and the velocity of the
- * nearest point of the trajectory. \param pose desired position of the path
- *  \param vel desired velocity of the nearest pose on the path
- *  \return 3d vector velocity to command
- */
-Eigen::Vector3f Follower::calculate_vel(const Eigen::Vector3f pose,
-                              const Eigen::Vector3f vel,
-                              const Eigen::Vector3f &current_pose) {
-  Eigen::Vector3f vel_to_command = (pose - current_pose).normalized();
-  double vel_module = vel.norm() + velocity_error;
-  /**if(vel_module<0.15){
-      vel_module = 0.15;
-  }
-   std::cout<<vel_module<<std::endl;*/
-  return vel_to_command * vel_module;
-}
 
 int main(int _argc, char **_argv) {
   ros::init(_argc, _argv, "trajectory_follower_node");
@@ -129,27 +62,35 @@ int main(int _argc, char **_argv) {
         follower.pose_on_path = 0;
         last_traj_received.positions.clear();
         last_traj_received.velocities.clear();
-        geometry_msgs::Twist velocity;
-        geometry_msgs::PoseStamped position;
+        last_traj_received.orientations.clear();
+        
+        Eigen::Vector3f velocity;
+        Eigen::Vector3f position;
+        Eigen::Quaternionf orientation;
 
+        geometry_msgs::PoseStamped pose_stamped;
         nav_msgs::Path path_to_publish;
         path_to_publish.header.frame_id = "map";
 
         for (auto &point : msg->points) {
-          position.pose.position.x = point.positions[0];
-          position.pose.position.y = point.positions[1];
-          position.pose.position.z = point.positions[2];
-          position.pose.orientation.x = point.positions[3];
-          position.pose.orientation.y = point.positions[4];
-          position.pose.orientation.z = point.positions[5];
-          position.pose.orientation.w = point.positions[6];
+          pose_stamped.pose.position.x = point.positions[0];
+          pose_stamped.pose.position.y = point.positions[1];
+          pose_stamped.pose.position.z = point.positions[2];
+          position[0] = point.positions[0];
+          position[1] = point.positions[1];
+          position[2] = point.positions[2];
+          orientation.x() = point.positions[3];
+          orientation.y() = point.positions[4];
+          orientation.z() = point.positions[5];
+          orientation.w() = point.positions[6];
 
-          velocity.linear.x = point.velocities[0];
-          velocity.linear.y = point.velocities[1];
-          velocity.linear.z = point.velocities[2];
+          velocity[0] = point.velocities[0];
+          velocity[1] = point.velocities[1];
+          velocity[2] = point.velocities[2];
           last_traj_received.velocities.push_back(velocity);
           last_traj_received.positions.push_back(position);
-          path_to_publish.poses.push_back(position);
+          last_traj_received.orientations.push_back(orientation);
+          path_to_publish.poses.push_back(pose_stamped);
         }
         tracking_pub.publish(path_to_publish);
       };
@@ -201,37 +142,25 @@ int main(int _argc, char **_argv) {
         break;
       }
       ROS_INFO("Follower: look ahead: %d", target_pose);
-      Eigen::Vector3f pose_to_go = Eigen::Vector3f(
-          last_traj_received.positions[target_pose].pose.position.x,
-          last_traj_received.positions[target_pose].pose.position.y,
-          last_traj_received.positions[target_pose].pose.position.z);
+      Eigen::Vector3f pose_to_go = last_traj_received.positions[target_pose];
       Eigen::Vector3f vel_to_go;
      
       if (follower.pose_on_path == 0) { // if pose to go is the firs point
-        Eigen::Vector3f pose_to_go =
-            Eigen::Vector3f(last_traj_received.positions[0].pose.position.x,
-                            last_traj_received.positions[0].pose.position.y,
-                            last_traj_received.positions[0].pose.position.z);
+        Eigen::Vector3f pose_to_go = last_traj_received.positions[0];
         float dist = (pose_to_go - uav_state.current_pose).norm();
-        vel_to_go = Eigen::Vector3f(
-            last_traj_received.velocities[follower.pose_on_path].linear.x + dist,
-            last_traj_received.velocities[follower.pose_on_path].linear.y + dist,
-            last_traj_received.velocities[follower.pose_on_path].linear.z + dist);
+        vel_to_go = last_traj_received.velocities[follower.pose_on_path] + Eigen::Vector3f(dist,dist,dist);
       } else // if it is not the first point
-        vel_to_go = Eigen::Vector3f(
-            last_traj_received.velocities[follower.pose_on_path].linear.x,
-            last_traj_received.velocities[follower.pose_on_path].linear.y,
-            last_traj_received.velocities[follower.pose_on_path].linear.z);
+        vel_to_go = last_traj_received.velocities[follower.pose_on_path];
 
       Eigen::Vector3f velocity_to_command =
           follower.calculate_vel(pose_to_go, vel_to_go, uav_state.current_pose);
 
       // yaw
       tf2::Quaternion desired_q(
-          last_traj_received.positions[target_pose].pose.orientation.x,
-          last_traj_received.positions[target_pose].pose.orientation.y,
-          last_traj_received.positions[target_pose].pose.orientation.z,
-          last_traj_received.positions[target_pose].pose.orientation.w);
+          last_traj_received.orientations[target_pose].x(),
+          last_traj_received.orientations[target_pose].y(),
+          last_traj_received.orientations[target_pose].z(),
+          last_traj_received.orientations[target_pose].w());
       tf2::Quaternion current_q(
           uav_state.current_orientation.x, uav_state.current_orientation.y,
           uav_state.current_orientation.z, uav_state.current_orientation.w);
@@ -256,6 +185,44 @@ int main(int _argc, char **_argv) {
     ros::spinOnce();
     ros::Duration(1).sleep();
   }
-
   return 0;
+}
+
+
+
+float Follower::calculateYawDiff(const float _desired_yaw, const float _current_yaw) {
+  float yaw_diff = _desired_yaw - _current_yaw;
+  while (yaw_diff < -M_PI) yaw_diff += 2 * M_PI;
+  while (yaw_diff > M_PI) yaw_diff -= 2 * M_PI;
+  return yaw_diff;
+}
+
+int Follower::cal_pose_on_path(const std::vector<Eigen::Vector3f> &positions,
+                     const Eigen::Vector3f &current_pose) {
+  double min_distance = INFINITY;
+  int pose_on_path_id = 0;
+  for (int i = pose_on_path; i < positions.size(); i++) {
+    if ((current_pose - positions[i]).norm() < min_distance) {
+      min_distance = (current_pose - positions[i]).norm();
+      pose_on_path_id = i;
+    }
+  }
+  return pose_on_path_id;
+}
+
+
+int Follower::cal_pose_look_ahead(
+    const std::vector<Eigen::Vector3f> &positions) {
+  for (int i = pose_on_path; i < positions.size(); i++) {
+    if ((positions[i]- positions[pose_on_path]).norm() > look_ahead) return i;
+  }
+  return positions.size();
+}
+
+Eigen::Vector3f Follower::calculate_vel(const Eigen::Vector3f &pose,
+                              const Eigen::Vector3f &vel,
+                              const Eigen::Vector3f &current_pose) {
+  Eigen::Vector3f vel_to_command = (pose - current_pose).normalized();
+  double vel_module = vel.norm() + velocity_error;
+  return vel_to_command * vel_module;
 }
