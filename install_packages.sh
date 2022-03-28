daddcffeda8521941cefe8f434d947887d23c020
damianpm98 <damianperez8@gmail.com>
2022-03-28 09:38:33 +0200

    Updated install packages for noetic

--- install_packages.sh
+++ install_packages.sh
@@ -5,16 +5,16 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 WORKSPACE_PATH="$( cd .. ; pwd -P )"
 
 
-## ROS Melodic installation
+## ROS noetic installation
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+sudo apt install ros-noetic-desktop-full
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install python-rosdep
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
 sudo rosdep init
 rosdep update
 
