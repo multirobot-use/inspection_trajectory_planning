df238e297de263d62458db3218ab0ab7f5bd1dd0
damianpm98 <damianperez8@gmail.com>
2022-06-07 11:57:56 +0200

    Separating install files for U18 and U20

--- install_packages_u20_noetic.sh
+++ install_packages_u20_noetic.sh
@@ -30,7 +30,6 @@ fi
 
 source ~/.bashrc
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install -y python3-rosdep
 sudo rosdep init
 rosdep update
 
